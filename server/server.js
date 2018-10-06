1b4dfdbec4da961196ac1b56f3e7a31bab56d7c8
Hung Tran <hungtrn75@gmail.com>
2018-10-06 22:40:22 +0700

    graphql

--- server/server.js
+++ server/server.js
@@ -37,6 +37,16 @@ app.use(
   })
 );
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "client", "build", "index.html")
+    );
+  });
+}
+
 const webpackMiddleware = require("webpack-dev-middleware");
 const webpack = require("webpack");
 const webpackConfig = require("../webpack.config.js");
